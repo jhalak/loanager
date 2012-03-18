b9dcb89cdf0f53a1ebea4e954dff2e0096dcd9ba
jhalak <jhalak@tasawr.com>
2012-03-18 14:57:40 +0600

    Done configuring the default route to home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loanager::Application.config.secret_token = 'b675f1d82d48341fd2dbcb36f4cee88936993af97d50b76eab461c6eb6957f8b6fef8afdfe910bef4f83b078e1bd486d09402541bf3e92a4fd925299f657bca3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Loanager::Application.config.session_store :cookie_store, :key => '_loanager_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Loanager::Application.config.session_store :active_record_store
