8d210667f95fe1d17c06e1d892318a2be3d1cebe
jhalak <jhalak@tasawr.com>
2012-04-09 15:27:33 +0600

    Fixed the problem with js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
-$('document').ready(function() {
-	$('.dropdown-toggle').dropdown();	
+jQuery.noConflict();
+jQuery('document').ready(function() {
+	jQuery('.dropdown-toggle').dropdown();
 });
